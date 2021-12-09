f26c5027cb041d56c9e3b0c58b04f8fca524cef8
Laakiin <anakinmrq@gmail.com>
2021-12-09 07:50:34 +0100

    NewVer.(Vincent)

--- app/src/main/java/com/anakinmrq/scheduleandgradesiutb/MainActivity.java
+++ app/src/main/java/com/anakinmrq/scheduleandgradesiutb/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.anakinmrq.scheduleandgradesiutb.activities.GradesActivity;
+import com.anakinmrq.scheduleandgradesiutb.activities.InfosActivity;
+import com.anakinmrq.scheduleandgradesiutb.activities.ScheduleActivity;
+import com.anakinmrq.scheduleandgradesiutb.activities.SettingsActivity;
+import com.anakinmrq.scheduleandgradesiutb.managers.FilesManager;
+import com.anakinmrq.scheduleandgradesiutb.managers.ProfilesManager;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,27 +22,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FilesManager.setupFiles(new File(getFilesDir().getAbsolutePath()));
         getSupportActionBar().hide();
     }
 
     public void schedule(View v){
-        Intent scheduleIntent = new Intent(this, Schedule.class);
+        Intent scheduleIntent = new Intent(this, ScheduleActivity.class);
         this.startActivity(scheduleIntent);
     }
     public void settings(View v){
-        Intent scheduleIntent = new Intent(this, Settings.class);
+        Intent scheduleIntent = new Intent(this, SettingsActivity.class);
         this.startActivity(scheduleIntent);
     }
     public void infos(View v){
-        Intent scheduleIntent = new Intent(this, Infos.class);
+        Intent scheduleIntent = new Intent(this, InfosActivity.class);
         this.startActivity(scheduleIntent);
     }
     public void grades(View v){
-        Intent scheduleIntent = new Intent(this, Grades.class);
-        this.startActivity(scheduleIntent);
-    }
-    public void siteIUTB(View v){
-        Intent scheduleIntent = new Intent(this, iutbSite.class);
+        if(ProfilesManager.getCurrentProfile() == null){
+            Toast.makeText(getApplicationContext(), "Aucun profil sélectionné", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Intent scheduleIntent = new Intent(this, GradesActivity.class);
         this.startActivity(scheduleIntent);
     }
 
